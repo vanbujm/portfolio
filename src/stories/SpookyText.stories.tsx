3b55aa3399e6b479aa32eaed0e9667f5f4215473
Jonathan Van Buren <vanbujm@gmail.com>
2021-01-05 15:11:32 +0800

    uhh maybe some progress

---
+++ src/stories/SpookyText.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { SpookyText } from './SpookyText';
+
+export default {
+  title: 'Example/Spooky Text',
+  component: SpookyText,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<{}> = (args) => <SpookyText {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Spooky Text',
+};
